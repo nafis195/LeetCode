038d2be73519a0fa25aefd2eb82fb80fb61a99f4
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-17 22:27:58 -0800

    Add files via upload

--- Two Sum/Source.cpp
+++ Two Sum/Source.cpp
@@ -1,1 +1,27 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// LeetCode - Two Sum
+// Problem Statement - https://leetcode.com/problems/two-sum/
+// Main.cpp
 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> temp;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    temp.push_back(i);
+                    temp.push_back(j);
+                }
+            }
+        }
+
+        return temp;
+    }
+};
