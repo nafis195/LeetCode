2f6f6c485c46b78f33878968fc61cd9aaf23cc1a
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-02-12 22:45:18 -0800

    Add files via upload

--- Two Sum II/Source.cpp
+++ Two Sum II/Source.cpp
@@ -1,1 +1,35 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Leetcode - 167 - Two Sum II
+// Problem Statement - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// Main.cpp
 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector <int> answer;
+
+        int startIdx, endIdx, sum = 0;
+
+        startIdx = 0;
+        endIdx = numbers.size() - 1;
+
+        while (startIdx < endIdx)
+        {
+            if (numbers[startIdx] + numbers[endIdx] == target)
+            {
+                answer.push_back(startIdx + 1);
+                answer.push_back(endIdx + 1);
+                break;
+            }
+            else if (numbers[startIdx] + numbers[endIdx] < target)
+            {
+                startIdx++;
+            }
+            else
+                endIdx--;
+        }
+        return answer;
+    }
+};
