5de5ae4a7eae3d6c0ec7d0695e3c17b6b2314029
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-02-18 00:47:52 -0800

    Add files via upload

--- Pow(x, n)/Source.cpp
+++ Pow(x, n)/Source.cpp
@@ -1,1 +1,13 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Leetcode - 50 - Pow(x, n)
+// Problem Statement - https://leetcode.com/problems/powx-n/
+// Main.cpp
 
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
