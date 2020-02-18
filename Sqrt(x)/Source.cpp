d9ce8fa9e75ab3114cdb7624fd123684bf993e3e
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-02-18 00:43:27 -0800

    Add files via upload

--- Sqrt(x)/Source.cpp
+++ Sqrt(x)/Source.cpp
@@ -1,1 +1,13 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Leetcode - 69 - Sqrt(x)
+// Problem Statement - https://leetcode.com/problems/sqrtx/
+// Main.cpp
 
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
