3813f58a4536d70e904121b0bdfc8ce1542b2d2e
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-04-24 13:23:35 -0700

    Add files via upload

--- Reverse Linked List/Source.cpp
+++ Reverse Linked List/Source.cpp
@@ -1,1 +1,35 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// LeetCode - 206 - Reverse Linked List
+// Problem Statement - https://leetcode.com/problems/reverse-linked-list/
+// Main.cpp
 
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* current = head;
+        ListNode* after = NULL;
+
+        while (current != NULL)
+        {
+            after = current->next;
+            current->next = prev;
+            prev = current;
+            current = after;
+        }
+        return prev;
+    }
+};
