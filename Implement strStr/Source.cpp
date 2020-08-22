d929168afbf9fa62bbe9d86775bf93bec4b45731
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-08-22 13:23:56 -0700

    Add files via upload

--- Implement strStr/Source.cpp
+++ Implement strStr/Source.cpp
@@ -1,1 +1,15 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Khan Chowdhury
+// LeetCode - 28 - Implement strStr
+// Problem Statement - https://leetcode.com/problems/implement-strstr/
+// Main.cpp
 
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int position = haystack.find(needle);
+        return position;
+    }
+};
