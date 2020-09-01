026fbe486cb31ee07946df55bc9703c051baa3dd
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-09-01 11:18:16 -0700

    Add files via upload

--- Intersection of Two Linked Lists/Source.cpp
+++ Intersection of Two Linked Lists/Source.cpp
@@ -1,1 +1,41 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Khan CHowdhury
+// LeetCode - 160 - Intersection of Two Linked Lists
+// Problem Statement - https://leetcode.com/problems/intersection-of-two-linked-lists/
+// Main.cpp
 
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* a_pointer = headA;
+        ListNode* b_pointer = headB;
+
+        if (a_pointer == NULL || b_pointer == NULL)
+            return NULL;
+
+        while (a_pointer != b_pointer)
+        {
+            if (a_pointer == NULL)
+                a_pointer = headB;
+            else
+                a_pointer = a_pointer->next;
+
+            if (b_pointer == NULL)
+                b_pointer = headA;
+            else
+                b_pointer = b_pointer->next;
+        }
+        return a_pointer;
+    }
+};
